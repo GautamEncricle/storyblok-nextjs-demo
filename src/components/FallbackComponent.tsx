94296c9fa11bc244a394b2d6b5b1b5ea4cb58fe8
Gautampatel917 <gautamm.encircle@gmail.com>
2025-07-30 09:48:02 +0530

    Update FallbackComponent.tsx: replace 'blok' with '&apos;blok&apos;' in error message

--- src/components/FallbackComponent.tsx
+++ src/components/FallbackComponent.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const FallbackComponent = () => {
   return (
     <div style={{ padding: "1rem", border: "1px solid red", color: "red", textAlign: "center" }}>
-      <p>Component not found or missing 'blok' property.</p>
+      <p>Component not found or missing &apos;blok&apos; property.</p>
     </div>
   );
 };
